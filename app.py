3f5c8155676fb87f59195e7461dd55b2ea9cfda6
David Laborico <david97132@hotmail.com>
2022-05-15 14:40:20 -0500

    Added basic door styling

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", greetings = "Hello from Flask")
+    return render_template("index.html", door_amount = 3)
     
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
